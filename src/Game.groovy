ffddfa744c59994f419e8f98d0536b2fbb68afbc
ivanjensen <ivanjensen@192.168.2.101>
2010-06-26 15:05:30 -0700

    Go number one, following the kata as laid out (not making it up.

--- src/Game.groovy
+++ src/Game.groovy
@@ -1,3 +1,48 @@
 
 class Game {
+	
+	def rolls = new int[21]
+	def currentRoll = 0
+	
+	def roll(pins) {
+		rolls[currentRoll++] = pins
+	}
+	
+	def score() {
+		def score = 0
+		def frameIndex = 0
+		for (def frame = 0; frame < 10; frame++) {
+			if (isStrike(frameIndex)) {
+				score += 10 + strikeBonus(frameIndex)
+				frameIndex++
+			} else if (isSpare(frameIndex)) { 
+				score += 10 + spareBonus(frameIndex)
+				frameIndex += 2
+			} else {
+				score += sumOfBallsInFrame(frameIndex)
+				frameIndex += 2
+			}
+		}
+		score
+	}
+	
+	def sumOfBallsInFrame(frameIndex) {
+		rolls[frameIndex] + rolls[frameIndex+1]
+	}
+	
+	def strikeBonus(frameIndex) {
+		rolls[frameIndex + 1] + rolls[frameIndex + 2]
+	}
+	
+	def spareBonus(frameIndex) {
+		rolls[frameIndex+2]
+	}
+	
+	def isSpare(frameIndex) {
+		rolls[frameIndex] + rolls[frameIndex+1] == 10
+	}
+	
+	def isStrike(frameIndex) {
+		rolls[frameIndex] == 10
+	}
 }
--- src/GameTest.groovy
+++ src/GameTest.groovy
@@ -1,3 +1,50 @@
 
 class GameTest extends GroovyTestCase {
+	
+	Game game = new Game()
+	
+	def rollMany(n, pins) {
+		(1..n).each { 
+			game.roll(pins)
+		}
+	}
+	
+	void testGutterBallGame() {
+		rollMany(20, 0)
+		assertEquals 0, game.score()
+	}
+	
+	void testAllOnes() {
+		rollMany(20, 1)
+		assertEquals 20, game.score()
+	}
+	
+	void testOneSpare() {
+		rollSpare()
+		game.roll(3)
+		rollMany(17, 0)
+		assertEquals 16, game.score()
+	}
+	
+	void testOneStrike() {
+		rollStrike()
+		game.roll 3
+		game.roll 4
+		rollMany 16, 0
+		assertEquals 24, game.score()
+	}
+	
+	void testPerfectGame() {
+		rollMany(12, 10)
+		assertEquals(300, game.score())
+	}
+	
+	def rollSpare() {
+		game.roll(5)
+		game.roll(5)
+	}
+
+	def rollStrike() {
+		game.roll(10)
+	}
 }
