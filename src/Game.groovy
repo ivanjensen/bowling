2d3db21a40723a9da5a33b418d2dad13bec39176
ivanjensen <ivanjensen@192.168.2.101>
2010-06-27 10:48:54 -0700

    Much quicker now (memorized better) - did miss the game creation duplication Didn't put a limit on the array - effectively using a list.

--- src/Game.groovy
+++ src/Game.groovy
@@ -1,3 +1,49 @@
 
 class Game {
+	
+	def rolls = []
+	def currentRoll = 0
+	
+	def roll(pins) {
+		rolls << pins
+	}
+	
+	def score() {
+		def score = 0
+		def frameIndex = 0
+		for (def frame = 0; frame < 10; frame++) {
+			if (isStrike(frameIndex)) { 
+				score += 10 + strikeBonus(frameIndex)
+				frameIndex++
+			} else if (isSpare(frameIndex)) {
+				score += 10 + spareBonus(frameIndex)
+				frameIndex += 2
+			} else {
+				score += sumOfFrame(frameIndex)
+				frameIndex += 2
+			}
+		}
+		score
+	}
+	
+	private sumOfFrame(frameIndex) {
+		rolls[frameIndex] + rolls[frameIndex + 1]
+	}
+	
+	private spareBonus(frameIndex) {
+		rolls[frameIndex + 2]
+	}
+	
+	private strikeBonus(frameIndex) {
+		rolls[frameIndex + 1] + rolls[frameIndex + 2]
+	}
+	
+	private isStrike(frameIndex) {
+		rolls[frameIndex] == 10
+	}
+	
+	private isSpare(frameIndex) {
+		rolls[frameIndex] + rolls[frameIndex + 1] == 10
+	}
+	
 }
--- src/GameTest.groovy
+++ src/GameTest.groovy
@@ -1,3 +1,51 @@
 
 class GameTest extends GroovyTestCase {
+	
+	def Game g = new Game()
+	
+	private rollMany(n, pins) {
+		(1..n).each {
+			g.roll pins
+		}
+	}
+	
+	void testGutterBallGame() {
+		rollMany 20, 0
+		assertEquals 0, g.score()
+	}
+	
+	void testAllOnes() {
+		rollMany 20, 1
+		assertEquals 20, g.score()
+	}
+	
+	void testOneSpare() {
+		rollSpare()
+		g.roll 3
+		rollMany 17, 0
+		assertEquals 16, g.score()
+	}
+	
+	void testOneStrike() {
+		rollStrike()
+		g.roll 3
+		g.roll 4
+		rollMany 16, 0
+		assertEquals 24, g.score()		
+	}
+	
+	void testPerfectGame() {
+		rollMany 12, 10
+		assertEquals 300, g.score()
+	}
+	
+	private rollSpare() {
+		g.roll 5
+		g.roll 5		
+	}
+
+	private rollStrike() {
+		g.roll 10
+	}
+	
 }
